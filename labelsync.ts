64913fc2d67067e35a59bccd4f2a790afb7c5ac4
maticzav <matic.zavadlal@gmail.com>
2020-05-09 10:52:56 +0200

    setup labelsync repo

--- labelsync.ts
+++ labelsync.ts
@@ -1,83 +1,13 @@
 import { labelsync, repo, label, colors } from 'label-sync'
 
 /* Repository */
-import { prisma } from './repos/prisma'
-import { github } from './repos/github'
+import { resk } from './repos/resk'
+import { labelSync } from './repos/labelsync'
 
 /* Config */
 labelsync({
   repos: {
-    /* Check presets in the repos folder. */
-    // prisma,
-    // github,
-    /* Personalized repositories */
-    resk: repo({
-      config: {
-        removeUnconfiguredLabels: true,
-      },
-      labels: [
-        /* Bugs */
-        label({
-          name: 'bug/0-needs-info',
-          color: colors.danger,
-          description: 'More information is needed for reproduction.',
-          siblings: ['kind/bug'],
-        }),
-        label({
-          name: 'bug/1-repro-available',
-          color: colors.danger,
-          description: 'A reproduction exists and needs to be confirmed.',
-          siblings: ['kind/bug'],
-        }),
-        label({
-          name: 'bug/2-confirmed',
-          color: colors.danger,
-          description: 'We have confirmed that this is a bug.',
-          siblings: ['kind/bug'],
-        }),
-        /* Kind */
-        label({
-          name: 'kind/bug',
-          color: colors.neutral,
-          description: 'A reported bug.',
-        }),
-        label({
-          name: 'kind/regression',
-          color: colors.neutral,
-          description: 'A reported bug in functionality that used to work before.',
-        }),
-        label({
-          name: 'kind/feature',
-          color: colors.neutral,
-          description: 'A request for a new feature.',
-        }),
-        label({
-          name: 'kind/improvement',
-          color: colors.neutral,
-          description: 'An improvement to existing feature and code.',
-        }),
-        label({
-          name: 'kind/docs',
-          color: colors.neutral,
-          description: 'A documentation change is required.',
-        }),
-        label({
-          name: 'kind/discussion',
-          color: colors.neutral,
-          description: 'Discussion is required.',
-        }),
-        label({
-          name: 'kind/question',
-          color: colors.neutral,
-          description: 'Developer asked a question. No code changes required.',
-        }),
-      ],
-    }),
-    'label-sync': repo({
-      config: {
-        removeUnconfiguredLabels: false,
-      },
-      labels: [...github],
-    }),
+    resk,
+    'label-sync': labelSync,
   },
 })
